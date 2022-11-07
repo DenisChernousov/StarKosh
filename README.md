aad5eafcbec888dae10888870b0de9a49b1f0b79
Denis <chernousovdv@gmail.com>
2022-11-07 08:49:07 +0700

    README upd

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-#GULP Guild mini version
-#----
+GULP Guild mini version
+
 Сборка без лишних модулей, для сборки простых сайтов
-#----
+
+для установки модулей напишите
+$ npm install del@6.1.1 sass gulp-sass gulp-autoprefixer gulp-group-css-media-queries gulp-include browser-sync
 Состав сборки:
--del чистим папку public
+-del(npm i del@6.1.1 важно поставить именно эту версию, 7 уже на ES) чистим папку public
 -sass & gulp-sass - подключаем SASS
 -gulp-autoprefixer - вендорные префиксы
 -gulp-group-css-media-queries - собираем медиа запросы в кучу
 -gulp-include - компонентный подход
 -browser-sync - локальный сервр
+
--- gulpfile.js
+++ gulpfile.js
@@ -21,10 +21,88 @@ function browsersync() {
 }
 
 function styles() {
-  return src('./src/styles/styles.scss')
+  return src('./src/styles/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({ grid: true }))
     .pipe(gcssmq())
     .pipe(dest('./public/css/'))
     .pipe(browserSync.stream())
 }
+
+function scripts() {
+  return src('./src/js/script.js')
+    .pipe(
+      includeFiles({
+        includePaths: './src/components/**/',
+      })
+    )
+    .pipe(dest('./public/js/'))
+    .pipe(browserSync.stream())
+}
+
+function pages() {
+  return src('./src/pages/*.html')
+    .pipe(
+      includeFiles({
+        includePaths: './src/components/**/',
+      })
+    )
+    .pipe(dest('./public/'))
+    .pipe(browserSync.reload({ stream: true, }))
+}
+
+function copyFonts() {
+  return src('./src/fonts/**/*')
+    .pipe(dest('./public/fonts/'))
+}
+
+function copyImages() {
+  return src('./src/images/**/*')
+    .pipe(dest('./public/images/'))
+}
+
+async function copyResources() {
+  copyFonts()
+  copyImages()
+}
+
+async function clean() {
+  return del.sync('./public/', { force: true })
+}
+
+function watch_dev() {
+  watch(['./src/js/script.js', './src/components/**/*.js'], scripts)
+  watch(['./src/styles/style.scss', './src/components/**/*.scss'], styles).on(
+    'change',
+    browserSync.reload
+  )
+  watch(['./src/pages/*.html', './src/components/**/*.html'], pages).on(
+    'change',
+    browserSync.reload
+  )
+}
+
+exports.browsersync = browsersync
+exports.clean = clean
+exports.scripts = scripts
+exports.styles = styles
+exports.pages = pages
+exports.copyResources = copyResources
+
+exports.default = parallel(
+  clean,
+  styles,
+  scripts,
+  copyResources,
+  pages,
+  browsersync,
+  watch_dev
+)
+
+exports.build = series(
+  clean,
+  styles,
+  scripts,
+  copyResources,
+  pages
+)
